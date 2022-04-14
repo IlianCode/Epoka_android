2f769e9f997bf86ef5627c71c39b5cdb13c61f24
IlianCode <benaissailian@outlook.fr>
2022-04-14 11:24:57 +0200

    ewi

--- app/src/main/java/com/example/epoka_android/MainActivity.java
+++ app/src/main/java/com/example/epoka_android/MainActivity.java
@@ -3,15 +3,30 @@ package com.example.epoka_android;
 import android.app.DatePickerDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,12 +34,14 @@ public class MainActivity extends AppCompatActivity {
     TextView tvDate2;
     DatePickerDialog.OnDateSetListener setListener;
 
+    //initialisation des variable de spinner
+    Spinner spinner;
+    TextView tvSpinner;
+    TextView textView6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         tvDate = findViewById(R.id.tv_date);
         tvDate2 = findViewById(R.id.tv_date2);
         Calendar calendar = Calendar.getInstance();
@@ -66,5 +83,76 @@ public class MainActivity extends AppCompatActivity {
                 datePickerDialog.show();
             }
         });
+
+        //  asignation des variables du spinner
+        spinner=findViewById(R.id.spinner);
+        tvSpinner = findViewById(R.id.tvSpinner);
+        //tableau pour le spinner
+        ArrayList<String> listeCommune = new ArrayList<>();
+        //ajout de commune
+        listeCommune.add("Choisir commune");
+        listeCommune.add("Grenoble");
+        listeCommune.add("Lyon");
+
+        //set adaptater :
+        spinner.setAdapter(new ArrayAdapter<>(MainActivity.this,
+                android.R.layout.simple_spinner_dropdown_item,listeCommune));
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i == 0){
+                    Toast.makeText(getApplicationContext(),
+                            "Veuillez choisir une commune", Toast.LENGTH_SHORT).show();
+                    String ewi ="";
+                    tvSpinner.setText(ewi);
+                }else {
+                     //recup la valeur choisie
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
+    // pour JSON
+    public String getServerdataJSON() {
+        InputStream is = null;
+        String data = "";
+
+        // Autoriser les opérations réseau sur le thread principal
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        try {
+            URL url = new URL("http://localhost:3000/api/communes");
+            HttpURLConnection connexion = (HttpURLConnection) url.openConnection();
+            connexion.connect();
+            is = connexion.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            data = br.readLine();
+        } catch (Exception expt) {
+            Log.e("log_tag", "Erreur pendant la récupération des données : " + expt.toString());
+        };
+        return (data);
+    }
+
+    /*public boolean isJSONValid(String test) {
+        try {
+            new JSONObject(test);
+        } catch (JSONException ex) {
+            // edited, to include @Arthur's comment
+            // e.g. in case JSONArray is valid as well...
+            try {
+                new JSONArray(test);
+            } catch (JSONException ex1) {
+                return false;
+            }
+        }
+        return true;
+    }*/
 }
--- app/src/main/java/com/example/epoka_android/loginPage.java
+++ app/src/main/java/com/example/epoka_android/loginPage.java
@@ -1,16 +1,8 @@
 package com.example.epoka_android;
 
-import android.os.StrictMode;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
-import android.widget.EditText;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class loginPage extends AppCompatActivity {
 
@@ -22,37 +14,7 @@ public class loginPage extends AppCompatActivity {
         button_connexion = findViewById(R.id.button_connexion);
 
     }
-    public void connexionTest(){
-        EditText editTextUsername;
-        EditText editTextPassword;
-        editTextUsername = findViewById(R.id.editTextUsername);
-        editTextPassword = findViewById(R.id.editTextTextPassword);
-
-        String urlServiceWeb = "http://172.16.170.105/api/connexion/" + editTextUsername.getText() + "/"+ editTextPassword.getText();
 
 
 
-    }
-    private String getServerDataTexteBrut(String urlString){
-        InputStream is = null;
-        String ch ="";
-        //autoriser les opé sur le thread principal
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-
-        try{
-            URL url = new URL(urlString);
-            HttpURLConnection connexion = (HttpURLConnection)url.openConnection();
-            connexion.connect();
-            is = connexion.getInputStream();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            String ligne;
-            while((ligne = br.readLine()) != null){
-                ch =ch + ligne;
-            };
-        }catch (Exception expt){
-        };
-        return(ch);
-    }
 }
